b43a77e42d9dc59d190f74324241e7ab7749fe38
unknown <angjiahui90@gmail.com>
2022-08-30 10:40:11 +0800

    switch to connect to local db

--- server.js
+++ server.js
@@ -28,10 +28,25 @@ app.use('/api/v1/jobs', jobsRouter)
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 const port = process.env.PORT || 5000
+const url = "mongodb://127.0.0.1:27017/admin"
 
 const start = async ()=>{
     try {
-        await connectDB (process.env.MONGO_URL)
+        // await connectDB (process.env.MONGO_URL)
+        // await connectDB("localhost:27017/admin")
+        (async () => {
+            try {
+                await connectDB (url, {
+                    useNewUrlParser: true,
+                    useUnifiedTopology: true,
+                    useFindAndModify: false,
+                    useCreateIndex: true
+                });
+                console.log(`MongoDB Connected: ${url}`);
+            } catch (err) {
+                console.error(err);
+            }
+        })();
         app.listen(port,()=>{
             console.log(`Server is listening on port ${port}...`)
         }) 
